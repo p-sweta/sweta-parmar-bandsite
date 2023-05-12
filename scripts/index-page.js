567d22647a04dcde3521c3432ff17f2679003c7c
Sweta <spmac@swetas-mbp.lan>
2023-05-12 00:53:49 -0400

    add .post method to get comments from the form

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -19,12 +19,9 @@
 //   },
 // ];
 
-
 api_key: "7d36c33f-9d98-40cc-a580-2c9c3af42073";
 
 const comments = [];
-
-
 
 //adding a section for comments
 // const conversation = document.querySelector('.conversation');
@@ -79,7 +76,6 @@ const comments = [];
 // }
 //**************/
 
-
 //function for getting today's date
 const date = (date) => {
   let mm = date.getMonth() + 1;
@@ -97,57 +93,91 @@ const date = (date) => {
 
 //function for adding a new comment
 const displayComment = (comments) => {
-  comments.forEach(comment =>{
+  comments.forEach((comment) => {
+    const commentSectionEl = document.querySelector(".comments");
+    //<div class="comments__container">
+    const commentsContainerEl = document.createElement("div");
+    commentsContainerEl.classList.add("comments__container");
+    commentSectionEl.appendChild(commentsContainerEl);
 
-  const commentSectionEl = document.querySelector(".comments");
-  //<div class="comments__container">
-  const commentsContainerEl = document.createElement("div");
-  commentsContainerEl.classList.add("comments__container");
-  commentSectionEl.appendChild(commentsContainerEl);
+    // <div class="comments__image-placeholder"></div>
+    const imagePlaceholderEl = document.createElement("div");
+    imagePlaceholderEl.classList.add("comments__image-placeholder");
+    commentsContainerEl.appendChild(imagePlaceholderEl);
 
-  // <div class="comments__image-placeholder"></div>
-  const imagePlaceholderEl = document.createElement("div");
-  imagePlaceholderEl.classList.add("comments__image-placeholder");
-  commentsContainerEl.appendChild(imagePlaceholderEl);
+    // <div class="comments__info">
+    const commentsInfoEl = document.createElement("div");
+    commentsInfoEl.classList.add("comments__info");
+    commentsContainerEl.appendChild(commentsInfoEl);
 
-  // <div class="comments__info">
-  const commentsInfoEl = document.createElement("div");
-  commentsInfoEl.classList.add("comments__info");
-  commentsContainerEl.appendChild(commentsInfoEl);
+    // <h4 class="comments__heading">name</h4>
+    const commentsHeadingEl = document.createElement("h4");
+    commentsHeadingEl.classList.add("comments__heading");
+    commentsHeadingEl.innerText = comment.name;
+    commentsInfoEl.appendChild(commentsHeadingEl);
 
-  // <h4 class="comments__heading">name</h4>
-  const commentsHeadingEl = document.createElement("h4");
-  commentsHeadingEl.classList.add("comments__heading");
-  commentsHeadingEl.innerText = comment.name;
-  commentsInfoEl.appendChild(commentsHeadingEl);
+    const dateEl = document.createElement("p");
+    let day = new Date(comment.date);
+    dateEl.innerText = date(day);
+    dateEl.classList.add("comments__date");
+    commentsInfoEl.appendChild(dateEl);
 
-  const dateEl = document.createElement("p");
-  let day = new Date(comment.date);
-  dateEl.innerText = date(day);
-  dateEl.classList.add("comments__date");
-  commentsInfoEl.appendChild(dateEl);
-
-  const commentEl = document.createElement("p");
-  commentEl.classList.add("comments__comment");
-  commentEl.innerText = comment.comment;
-  commentsInfoEl.appendChild(commentEl);
-});
+    const commentEl = document.createElement("p");
+    commentEl.classList.add("comments__comment");
+    commentEl.innerText = comment.comment;
+    commentsInfoEl.appendChild(commentEl);
+  });
 };
 
-axios.get("https://project-1-api.herokuapp.com/comments?api_key=7d36c33f-9d98-40cc-a580-2c9c3af42073")
-  .then(response => {
-    console.log(response.data);
-    response.data.forEach(comment => {
-      const commentObj = {name: comment.name, comment: comment.comment, date: comment.timestamp};
-      comments.push(commentObj);
-    }) 
-    
-  }).then(results => {
-    console.log(comments);
-    displayComment(comments);
-  })
-  // formEl.reset();
-
 //event listener for adding comments on submit
+const addNewComment = (e) => {
+  e.preventDefault();
+  const name = e.target.fullName.value;
+  const comment = e.target.comment.value;
+  // console.log(comment, name);
+  axios.post(
+    "https://project-1-api.herokuapp.com/comments?api_key=7d36c33f-9d98-40cc-a580-2c9c3af42073",
+    { name: name, comment: comment }
+  )
+  .then((resonse) => {
+    addComments();
+  })
+  const formEl = document.querySelector("form");
+  formEl.reset();
+};
+
+const formEl = document.querySelector("form");
+formEl.addEventListener("submit", addNewComment);
+
+const addComments = () => {
+axios.get(
+    "https://project-1-api.herokuapp.com/comments?api_key=7d36c33f-9d98-40cc-a580-2c9c3af42073"
+  )
+  .then((response) => {
+    console.log(response.data);
+    response.data.forEach((comment) => {
+      const commentObj = {
+        name: comment.name,
+        comment: comment.comment,
+        date: comment.timestamp,
+      };
+      comments.push(commentObj);
+    });
+  })
+  .then((results) => {
+    console.log(comments);
+    displayComment(
+      comments.sort((a, b) => {
+        return b.date - a.date;
+      })
+    );
+  });
+}
+
+addComments();
+
+
+
 // const formEl = document.querySelector("form");
-// formEl.addEventListener("submit", displayComment);
+// formEl.addEventListener("submit", )
+
